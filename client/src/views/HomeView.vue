55a34826e5a408156f087d4a56e34fa4e50dbdf6
leireomadina <leireomadina@gmail.com>
2024-04-28 11:05:38 +0200

    Add home view

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <h1>Pokéapi</h1>
+  </header>
+  <main>
+    <header>
+      <img src="@/assets/logo.svg" />
+    </header>
+    <section>Cards</section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  display: none;
+}
+</style>
