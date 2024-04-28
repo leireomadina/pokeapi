f05dad081e2e73572894ef973a08b92aba091089
leireomadina <leireomadina@gmail.com>
2024-04-28 13:35:51 +0200

    Order pokemons by id

--- client/src/components/CardList.vue
+++ client/src/components/CardList.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 import CardItem from './CardItem.vue'
 import pokemonsJson from '@/data/pokemons.json'
+
+interface Pokemon {
+  id: string;
+  name: string;
+  image: string;
+  color: string;
+  types: { name: string; url: string }[];
+}
+
+const pokemons: Pokemon[] = pokemonsJson.species
+
+function orderPokemonsById() {
+  return [...pokemons].sort((a, b) => {
+    return parseInt(a.id) - parseInt(b.id)
+  })
+}
+const orderedPokemons = orderPokemonsById()
 </script>
 
 <template>
 	<ul class="list">
-		<li v-for="(pokemon, index) in pokemonsJson.species" :key="index">
+		<li v-for="(pokemon, index) in orderedPokemons" :key="index">
 			<CardItem :id="pokemon.id" :title="pokemon.name" :image="pokemon.image" :color="pokemon.color" />
 		</li>
 	</ul>
