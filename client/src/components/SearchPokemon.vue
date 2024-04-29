4bcdf9d2b82a29a31ed1b695fe2ca0469f0d287a
leireomadina <leireomadina@gmail.com>
2024-04-29 18:05:00 +0200

    Add search pokemon component

--- client/src/components/SearchPokemon.vue
+++ client/src/components/SearchPokemon.vue
@@ -1,1 +1,43 @@
-<template>Search Pokemon</template>
+<script setup lang="ts">
+import { defineEmits } from 'vue'
+import pokemonsJson from '@/data/pokemons.json'
+
+interface Pokemon {
+  id: string;
+  name: string;
+  image: string;
+  color: string;
+  types: { name: string; url: string }[];
+}
+
+const pokemons: Pokemon[] = pokemonsJson.species
+const pokemonNames = pokemons.map((pokemon) => {
+  return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+})
+
+const emit = defineEmits(['filterPokemonByName'])
+
+function handlePokemonNameChange(event: Event) {
+	const selectedName = (event.target as HTMLInputElement).value
+	emit('filterPokemonByName', selectedName)
+}
+</script>
+
+<template>
+	<v-autocomplete
+		label="Search a Pokémon"
+		:items="pokemonNames"
+		append-inner-icon="mdi-microphone"
+		class="mx-auto"
+		density="comfortable"
+		placeholder="Type a name"
+		style="max-width: 350px;"
+		theme="light"
+		variant="solo"
+		auto-select-first
+		item-props
+		rounded
+		item-color="blue"
+		@input="handlePokemonNameChange"
+	></v-autocomplete>
+</template>
